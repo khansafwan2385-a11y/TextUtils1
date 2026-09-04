b3cb2de513be059126196877992569e9c2ac3d12
khansafwan2385-a11y <khansafwan2385@gmail.com>
2026-09-04 21:50:32 +0530

    Refactor Navbar component imports

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 
 import PropTypes from 'prop-types'
-import React,{useState} from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 export default function Navbar({Title,changeMode}) {  
   return (
